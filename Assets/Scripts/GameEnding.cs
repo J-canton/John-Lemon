87b9496b36f323f8b29aa7754ca01d113305fa85
Juan Luis Cantón Garay <juanluiscantongaray@iMac-de-Juan.local>
2020-12-21 18:18:45 +0100

    finish day

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundCanvasGroup;
+    private float timer;
+    private bool isPlayerExit;
+
+
+    private void Update()
+    {
+        if (isPlayerExit)
+        {
+            timer += Time.deltaTime;
+            exitBackgroundCanvasGroup.alpha = Mathf.Clamp(timer / fadeDuration, 0,1);
+
+            if (timer > fadeDuration + displayImageDuration)
+            {
+                EndLevel();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            isPlayerExit = true;
+        }
+    }
+
+    void EndLevel()
+    {
+        Application.Quit();
+    }
+}
